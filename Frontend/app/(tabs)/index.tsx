1786cdc58a9b423414da6e20ebddb421626c63b2
paulto <tovanhuong007@gmail.com>
2025-02-13 15:37:09 +0700

    complete login

--- Frontend/app/(tabs)/index.tsx
+++ Frontend/app/(tabs)/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native';
+import LoginScreen from '../login';
 
 const index = () => {
     return (
         <SafeAreaView>
-            <View>
+            <View className='h-[500px] flex justify-center items-center'>
                 <Text className='text-red-600 font-bold text-4xl'>Hello worlds</Text>
+                <LoginScreen />
             </View>
         </SafeAreaView>
     );
