bf743ca38cdff8d01eb2d01743bf089b7e345a28
paulto <tovanhuong007@gmail.com>
2025-02-13 08:24:10 +0700

    complete initialize project

---
+++ Frontend/app/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native';
+
+const index = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text className='text-red-600 font-bold text-4xl'>Hello worlds</Text>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default index;
