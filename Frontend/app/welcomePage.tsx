ff6a38e85ca7d8fb52234b2037e7d7a3d69e1091
LeHuuHoangAnh-200703 <lehuuhoanganhhg2003@gmail.com>
2025-02-14 08:05:20 +0700

    xong welcome

--- Frontend/app/welcomePage.tsx
+++ Frontend/app/welcomePage.tsx
@@ -5,13 +5,13 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function welcomePage() {
     return (
         <SafeAreaView>
-            <View className='relative bg-[#9661D9] w-full h-full items-center'>
+            <View className='relative bg-[#9661D9] w-full min-h-screen items-center'>
                 <View className="items-center mt-24">
                     <Image style={{ width: 300, height: 40 }} source={require('../assets/images/2HAND MARKET.png')} />
                     <Image className="w-[350px] h-[350px]" source={require('../assets/images/image 2.png')} />
                 </View>
-                <Image className='absolute bottom-[-70px]' source={require('../assets/images/Vector 8.png')} />
-                <View className='absolute bottom-12 flex-row justify-center items-center gap-10'>
+                <Image style={{ width: '100%' }} className='absolute bottom-0' source={require('../assets/images/Vector 8.png')} />
+                <View className='absolute bottom-28 flex-row justify-center items-center gap-10'>
                     <TouchableOpacity className='border-4 border-[#9661D9] px-10 py-3 rounded-2xl'>
                         <Text className='text-[#9661D9] text-center text-[20px] font-bold'>Đăng ký</Text>
                     </TouchableOpacity>
