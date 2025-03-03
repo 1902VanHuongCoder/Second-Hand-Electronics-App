66b0574b729b3887fc16c09f5f1bc6a6186fd27a
LeHuuHoangAnh-200703 <lehuuhoanganhhg2003@gmail.com>
2025-03-03 14:57:36 +0700

    doi nhanh

--- routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('../controllers/productController');
-
-// Search products
-router.get('/search', productController.searchProducts);
-
-module.exports = router; 
